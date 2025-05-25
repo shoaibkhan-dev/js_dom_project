ed9a342819a0180a44f83bc98c286c47dd8051bf
Shoaib khan <shoaibmangal@yahoo.com>
2025-05-25 21:51:46 +0500

    add files

---
+++ app.js
@@ -0,0 +1,18 @@
+const istatus = document.querySelector("h4");
+const addFriend = document.querySelector("#add");
+const removeFriend = document.querySelector("#remove");
+
+addFriend.addEventListener("click", function(){
+    istatus.innerHTML = "Friends"
+    istatus.style.color = "green"
+   if(istatus === addFriend){
+    alert("add to friends");
+   }
+    
+});
+
+removeFriend.addEventListener("click", function(){
+    istatus.innerHTML = "Stranger"
+    istatus.style.color = "red"
+})
+
